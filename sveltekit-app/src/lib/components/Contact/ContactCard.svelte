0ecebb636113ad7c545a9aac24c18fb83ee4caa3
Celestin <hello@celest.in>
2023-03-21 19:27:55 +0100

    pushing to deta

---
+++ sveltekit-app/src/lib/components/Contact/ContactCard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let name: string;
+	export let number: string;
+	export let email: string;
+
+	let showDetails = false;
+
+</script>
+<div class="w-full border border-slate-200">
+	<head>
+		<h2>
+			{name}	
+		</h2>
+		<button on:click={() => showDetails = !showDetails}>
+			{showDetails ? "Hide" : "Show"}
+		</button>
+	</head>
+	<main>
+		{#if showDetails}
+			<div class="w-full">
+				<p>{number}</p>
+				<p>{email}</p>
+			</div>
+		{/if}
+	</main>
+</div>
--- sveltekit-app/src/routes/+page.svelte
+++ sveltekit-app/src/routes/+page.svelte
@@ -2,9 +2,7 @@
 
 </script>
 
-<div class="bg-red-300 font-serif p-8">
-  <h1>Welcome to your library project</h1>
-  <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-full h-full bg-red-300/20 font-serif p-8 overflow-y-auto">
+  
 </div>
 
